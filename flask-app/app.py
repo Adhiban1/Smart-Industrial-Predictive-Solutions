940a0b67ce0ba9ae33a6004151bbd3106a0528bb
Adhiban1 <siddhu1131@gmail.com>
2023-09-23 22:13:38 +0530

    updated

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
@@ -8,25 +8,15 @@ def index():
 
 @app.route('/machine', methods=['POST', 'GET'])
 def machine():
-    data = [
-        ('A'*10, 1),
-        ('B'*10, 2),
-        ('C'*10, 3)
-    ]
-    if request.method == 'POST':
-        print(request.form)
-    return render_template('machine.html', data=data)
-
-@app.route('/water', methods=['POST', 'GET'])
-def water():
-    data = [
-        ('A'*10, 1),
-        ('B'*10, 2),
-        ('C'*10, 3)
-    ]
-    if request.method == 'POST':
-        print(request.form)
-    return render_template('water.html', data=data)
+    if request.method == 'POST' and 'file' in request.files:
+        f = request.files['file']
+        f.save('uploaded/input.csv')
+        return send_file('uploaded/input.csv', 
+                         as_attachment=True, 
+                         download_name='output.csv')
+    else:
+        print('No files or no post method')
+    return render_template('machine.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='localhost', port=5000)
